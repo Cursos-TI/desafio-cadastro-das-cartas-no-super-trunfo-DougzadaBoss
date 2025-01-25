72109aefa6b5558bb13849aa9cd6ca623eec4863
DougzadaBoss <dougfany1907@gmail.com>
2025-01-25 17:47:54 +0000

    super trunfo nivel 2 completo

--- SUPERTRUNFO.c
+++ SUPERTRUNFO.c
@@ -3,10 +3,11 @@ int main(){
     //Definição das váriaveis
     int codigo_da_cidade;
     char nome[40];
-    float populacao;
-    float area;
-    float pib;
+    int populacao;
+    int area;
+    int pib;
     int pontos_turisticos;
+    float densidade,pib_percapta;
 
     //inicio da cadeia de comando interativa para coletar dados do usuário
     printf("Olá, bem vindo ao super trunfo. Para começar digite o nome da cidade:\n");
@@ -16,25 +17,31 @@ int main(){
     scanf("%d",&codigo_da_cidade);
     
     printf("Agora digite a população da cidade:\n");
-    scanf(" %f", &populacao);
+    scanf(" %d", &populacao);
     
     printf("Agora por favor digite o PIB da cidade:\n"); 
-    scanf(" %f", &pib);
+    scanf(" %d", &pib);
     
     printf("Digite agora a área da cidade:\n");
-    scanf(" %f", &area);
+    scanf(" %d", &area);
     
     printf("E por último digite agora os pontos turísticos:\n");
     scanf(" %d", &pontos_turisticos);
 
     //exibição dos dados coletados em tela
     printf("Nome da cidade: %s\n",nome);
-    printf("O código da cidade é: %d\n",codigo_da_cidade);
-    printf("A população total da cidade é: %f\n", populacao);
-    printf("O PIB da cidade é: %f\n", pib);
-    printf("A área total da cidade é: %f\n", area);
-    printf("A quantidade de pontos turísticos são: %d\n", pontos_turisticos);
+    printf("O código da cidade é: %d\n",codigo_da_cidade);  
+    printf("A população total da cidade é: %d\n", populacao);
+    printf("O PIB da cidade é: %d\n", pib);
+    printf("A área total da cidade é: %d\n", area);
+    printf("A quantidade de pontos turísticos são: %d\n", pontos_turisticos); 
 
+     densidade =(float) populacao / area;
+    printf("A densidade populacional é %f",densidade);
+    
+    pib_percapta = (float)pib / populacao;
+
+    printf("O PIB percapta é: %f", pib_percapta);
     
 return 0;
 }
